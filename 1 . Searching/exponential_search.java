ab549519a12b18a05ee08c5d63462b59031b0b25
Arnav Thakare <91930603+Arnavthakare19@users.noreply.github.com>
2023-10-08 13:12:50 +0530

    added exponential search
    Exponential search is a combination of binary search and linear search that works well for unbounded/infinite-sized arrays.

---
+++ 1 . Searching/exponential_search.java
@@ -0,0 +1,80 @@
+import java.util.*;
+public class exponential_search
+{
+    // Binary search algorithm to return the position of key `x` in
+    // subarray A[left…right]
+    private static int binarySearch(int[] A, int left, int right, int x)
+    {
+        // base condition (search space is exhausted)
+        if (left > right) {
+            return -1;
+        }
+ 
+        // find the mid-value in the search space and
+        // compares it with the key
+ 
+        int mid = (left + right) / 2;
+ 
+        // overflow can happen. Use
+        // int mid = left + (right - left) / 2;
+ 
+        // base condition (a key is found)
+        if (x == A[mid]) {
+            return mid;
+        }
+ 
+        // discard all elements in the right search space,
+        // including the middle element
+        else if (x < A[mid]) {
+            return binarySearch(A, left, mid - 1, x);
+        }
+ 
+        // discard all elements in the left search space,
+        // including the middle element
+        else {
+            return binarySearch(A, mid + 1, right, x);
+        }
+    }
+ 
+    // Returns the position of key `x` in a given array `A` of length `n`
+    public static int exponentialSearch(int[] A, int x)
+    {
+        // base case
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+ 
+        int bound = 1;
+ 
+        // find the range in which key `x` would reside
+        while (bound < A.length && A[bound] < x) {
+            bound *= 2;        // calculate the next power of 2
+        }
+ 
+        // call binary search on A[bound/2 … min(bound, n-1)]
+        return binarySearch(A, bound/2, Integer.min(bound, A.length - 1), x);
+    }
+ 
+    // Exponential search algorithm
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] A = new int[n];
+        System.out.println("Enter the elements : ");
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+        System.out.println("Enter the search element : ");
+        int key = sc.nextInt();
+ 
+        int index = exponentialSearch(A, key);
+ 
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        }
+        else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
